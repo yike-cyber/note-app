bb44ad2238edbb7d9c24c5444f60751ec1148dfd
yikeber <www.yikebermisganaw@gmail.com>
2024-05-28 13:48:56 +0300

    note model,getUrl view and url config is added

--- Backend/myNotes/api/views.py
+++ Backend/myNotes/api/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def getRoutes(request):
+    
+     routes = [
+        {
+            'EndPoint':'/notes',
+            'method':'GET',
+            'body':None,
+            'description':'returns an array of notes'
+        },
+        {
+            'EndPoint':'/notes/id',
+            'method':'GET',
+            'body':{'body':""},
+            'description':'return single note object'
+        },
+        {
+            'EndPoint':'/notes/create/',
+            'method':'POST',
+            'body':{'body':""},
+            'description':'creates new note with data sent in post request'
+        },
+        
+        {
+            'EndPoint':'/notes/update/',
+            'method':'PUT',
+            'body':{'body':""},
+            'description':'creates an existing  note with data sent in post request'
+        },
+        
+        {
+            'EndPoint':'/notes/delete',
+            'method':'DELETE',
+            'body':None,
+            'description':'deletes an existing note'
+        } 
+        
+    ]
+    
+     return JsonResponse(routes,safe=False)
