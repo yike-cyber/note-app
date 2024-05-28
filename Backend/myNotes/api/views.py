46da2cb867f85a9905cda76562ba0591edf06371
yikeber <www.yikebermisganaw@gmail.com>
2024-05-28 14:12:43 +0300

    NoteSerializer is added and getNotes view is added

--- Backend/myNotes/api/views.py
+++ Backend/myNotes/api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+from .models import Note
+from . serializers import NoteSerialezer
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
     
      routes = [
@@ -41,4 +45,18 @@ def getRoutes(request):
         
     ]
     
-     return JsonResponse(routes,safe=False)
+     return Response(routes)
+ 
+@api_view(['GET'])
+def getNotes(request):
+    
+    notes = Note.objects.all()
+    serializer = NoteSerialezer(instance= notes,many= True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request,pk):
+    
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerialezer(instance= note,many= False)
+    return Response(serializer.data)
